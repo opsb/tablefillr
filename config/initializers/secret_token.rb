e8549eae60aac2a049c64b997ec263c2f1eceda8
Oliver Searle-Barnes <oliver@opsb.co.uk>
2013-09-26 15:27:53 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tablefillr::Application.config.secret_key_base = '1cd3ce618bd0e0f7ce3d29b14bd741995bd02a1740fa71eb5a6dcd9dd026c853e54a0d15e6c80ad9952306553c653857bcc8c5792ab731748c54e8cdcb6ef9ff'
