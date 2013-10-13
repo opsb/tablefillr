d834ec3c054b35e07c15718a653fe2cf0a56e0c6
opsb <oliver@opsb.co.uk>
2013-10-13 22:07:22 +0100

    Move association method into model

--- app/models/layout.rb
+++ app/models/layout.rb
@@ -1,4 +1,8 @@
 class Layout < Template
 	validates_presence_of :name, :body
 	belongs_to :theme, :inverse_of => :pages
+
+	def self.main
+		find_by_name("main")
+	end
 end
--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,13 +1,8 @@
 class Theme < ActiveRecord::Base
 	has_many :restaurant_themes
 	has_many :restaurant, :through => :restaurant_themes
-
 	has_many :pages, autosave: true
-	has_many :layouts, autosave: true do
-		def main
-			self.find_by_name("main")
-		end
-	end
+	has_many :layouts, autosave: true
 	has_permalink :name
 
 	validates_presence_of :name
