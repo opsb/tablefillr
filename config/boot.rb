3871dd775b537db00ff2e8bb6712681f9121ff2a
opsb <oliver@opsb.co.uk>
2013-10-19 09:30:20 -0500

    Add support for SAFE_ENV
    Same as ENV except will raise an exception if no value is found

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,7 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+SAFE_ENV = Proc.new do |name|
+  ENV[name].tap{ |value| raise "Environment variable #{name} is missing" unless value }
+end
